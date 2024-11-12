3a178c1d374c874144c228a36225ec5e020e51b8
Mikel Rivera Guerrero <104358538+MRiveraGuerrero@users.noreply.github.com>
2024-11-12 16:06:12 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# suma.py
+
+# Definición de la función que suma dos números
+def sumar(num1, num2):
+    return num1 + num2
+
+# Variables con dos números elegidos
+numero1 = 5
+numero2 = 3
+
+# Imprimir el resultado de la suma usando la función
+print("La suma de", numero1, "y", numero2, "es:", sumar(numero1, numero2))
